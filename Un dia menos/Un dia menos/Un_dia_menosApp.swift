a65851cd473a3ec629a1f7ee03ba6264606ee8ff
Carlos <charlsef66@gmail.com>
2026-01-14 20:09:00 +0100

    first commit

---
+++ Un dia menos/Un dia menos/Un_dia_menosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Un_dia_menosApp.swift
+//  Un dia menos
+//
+//  Created by Carlos Esteve Fernández on 14/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Un_dia_menosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
